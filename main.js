89e52dbda98231867fd1c290a2d621fe506731b3
Nikola Milun <82384619+nikolamilun@users.noreply.github.com>
2022-01-28 12:53:29 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+let mainimage = document.querySelector('#mainimage');
+let imgcounter = 1;
+let tajmer = setInterval(nextImage, 5000);
+let rotationindexes = document.querySelectorAll('.rotation');
+let src;
+function rotateForward(){
+    rotationindexes[imgcounter - 1].classList.remove('active');
+    imgcounter++;
+    if(imgcounter == 7)
+        imgcounter = 1;
+    rotationindexes[imgcounter - 1].classList.add('active');
+    src = 'images/galerija0' + imgcounter + '.jpg';
+    mainimage.classList.add('fade');
+    setTimeout(function (){
+        mainimage.src = src;
+        mainimage.classList.remove('fade');
+    }, 1000);
+}
+function rotateBackwards(){
+    rotationindexes[imgcounter - 1].classList.remove('active');
+    imgcounter--;
+    if(imgcounter == 0)
+        imgcounter = 6;
+    rotationindexes[imgcounter - 1].classList.add('active');
+    src = 'images/galerija0' + imgcounter + '.jpg';
+    mainimage.classList.add('fade');
+    setTimeout(function (){
+        mainimage.src = src;
+        mainimage.classList.remove('fade');
+    }, 1000);
+}
+function nextImage(){
+    clearInterval(tajmer);
+    rotateForward();
+    tajmer = setInterval(nextImage, 5000);
+}
+function prevImage(){
+    clearInterval(tajmer);
+    rotateBackwards();
+    tajmer = setInterval(nextImage, 5000);
+}
